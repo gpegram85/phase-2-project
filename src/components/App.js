c9b19d4d1739b11124b85d1813b43f6b337c6918
Grey Pegram <dpegram85@gmail.com>
2024-08-08 21:46:21 -0400

    Basic Setup

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+
+  const [games, setGames] = useState([])
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/games`)
+    .then(resp => resp.json())
+    .then(data => setGames(data))
+  }, [])
+
+  const logGames = () => {
+    console.log("Fetched Games: ", games)
+  }
+
+  return (
+    <div className="App">
+      <p>Placeholder</p>
+      <button onClick={logGames}>Log Game List</button>
+    </div>
+  );
+}
+
+export default App;
